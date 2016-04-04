5cbad8b9324d38c89d038e0b7accb07d96138cc5
Alex Roetter <aroetter@Alexs-MacBook-Pro.local>
2016-04-04 12:02:47 -0700

    Initial Commit and some initial homework on kNN.ipydb and k nearest neighbors file in cs231n/classifiers

---
+++ aroetter_knn.py
@@ -0,0 +1,60 @@
+#!/Users/aroetter/anaconda/bin/python2.7
+
+
+import random
+import numpy as np
+from cs231n.data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+# This is a bit of magic to make matplotlib figures appear inline in the notebook
+# rather than in a new window.
+# %matplotlib inline
+
+
+# Some more magic so that the notebook will reload external python modules;
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+#%load_ext autoreload
+#%autoreload 2
+
+
+def VisualizeDataSetSamples(X_train, y_train):
+    # Visualize some examples from the dataset.
+    # We show a few examples of training images from each class.
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
+               'horse', 'ship', 'truck']
+    num_classes = len(classes)
+    samples_per_class = 7
+    for y, cls in enumerate(classes):
+        idxs = np.flatnonzero(y_train == y)
+        idxs = np.random.choice(idxs, samples_per_class, replace=False)
+        for i, idx in enumerate(idxs):
+            plt_idx = i * num_classes + y + 1
+            plt.subplot(samples_per_class, num_classes, plt_idx)
+            plt.imshow(X_train[idx].astype('uint8'))
+            plt.axis('off')
+            if i == 0:
+                plt.title(cls)
+    plt.show()
+
+
+def main():
+    plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'    
+
+    # Load data
+    # Load the raw CIFAR-10 data.
+    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # As a sanity check, we print out the size of the training and test data.
+    print 'Training data shape: ', X_train.shape
+    print 'Training labels shape: ', y_train.shape
+    print 'Test data shape: ', X_test.shape
+    print 'Test labels shape: ', y_test.shape
+
+    VisualizeDataSetSamples(X_train, y_train)
+
+    
+if __name__ == "__main__":
+    main()
