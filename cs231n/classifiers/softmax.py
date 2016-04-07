7f1f7578c4be6995136e90f7bf965ffe89f1c1b0
Alex Roetter <nothing@nothing.nothing>
2016-04-07 16:19:50 -0700

    got svm vectorized loss and dW working

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -51,8 +51,8 @@ def softmax_loss_naive(W, X, y, reg):
     # i just manually took the derivative of:
     # L_i = -f(yi) + log(sum over j(e^f(j))
     # which yields
-    # dW_ij = 1{j=y(i)} * -1 * x_i
-    #         + 1/(sum over j e^f(j)) * e^f(j)*x_i
+    # dW_ij = 1{j=y(i)} * -1 * x_i                  # dW_term1
+    #         + 1/(sum over j e^f(j)) * e^f(j)*x_i  # dW_term2
     # 
     for j in xrange(num_classes):
       dW[:, j] += (1 / denom) * math.exp(scores[i, j]) * X[i]
@@ -62,12 +62,9 @@ def softmax_loss_naive(W, X, y, reg):
   loss /= num_train
   dW /= num_train
 
-  # add in the regularization loss
+  # add in the regularization contributions
   loss += 0.5 * reg * np.sum(W * W)
   dW += reg * W
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
 
   return loss, dW
 
@@ -78,37 +75,36 @@ def softmax_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as softmax_loss_naive.
   """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
-
-  
-  ########
   num_train = X.shape[0]
+  num_classes = W.shape[1]
   # gives an N x C matrix.
   scores = X.dot(W)
-  # for numerical stability
+  # make largest value here zero, just for numeric stability
+  scores -=np.max(scores)
   expscores = np.exp(scores)
+
   denoms = expscores.sum(axis=1)
   numers = expscores[range(num_train), y]
-  vector_loss = -1 * np.log(numers / denoms).sum()
-  vector_loss /= num_train
-  print "vector loss (ignoring reg): ", vector_loss
+  loss = -1 * np.log(numers / denoms).sum()
   ########
+
+  #Now compute dW
+  # remember from above, the derivative with respect to w_ij, which we call
+  # dW_ij, is 2 terms. the first only triggers if the ith data pt is in the jth
+  # class, the 2nd term is a product of 3 things (see comments above in the naive
+  # impl to see what i mean by dW_term1 and dW_term2
+  correct_class = np.zeros((num_train, num_classes))
+  correct_class[xrange(num_train),y] = -1
+  dW_term1 = X.T.dot(correct_class)
+  dW_term2 = (X.T / denoms).dot(expscores)
+  dW = dW_term1 + dW_term2
+
+  loss /= num_train
+  dW /= float(num_train)
+
+  # add in the regularization contributions
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
   
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-
-
-
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-
   return loss, dW
 
